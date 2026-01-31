d00e460e4c3d51f86422af34f5d20e047224272f
Aleks Tacconi <aleks.tacconi@gmail.com>
2026-01-31 12:04:23 +0000

    Install steam on laptop

--- devices/laptop.nix
+++ devices/laptop.nix
@@ -12,6 +12,7 @@
     ./hardware-configuration-laptop.nix
     ../configuration/applications/eww/configuration-laptop.nix
     ../configuration/nixconfig/bluetooth.nix
+    ../configuration/applications/steam/configuration.nix
   ];
 
   systemd.tmpfiles.rules = [ "w /sys/class/leds/tpacpi::kbd_backlight/brightness - - - - 2" ];
